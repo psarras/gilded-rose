3f856d33ac2bfbcd219c7f37371520ba600bad35
spsarras <psarras.st@gmail.com>
2024-02-01 14:01:55 +0000

    qualityAdjustement

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -16,36 +16,40 @@ namespace csharp
         {
             foreach (var item in Items)
             {
+                int qualityAdjustement = 0;
                 if (item.Name == "Aged Brie")
                 {
-                    item.Quality = Math.Min(item.Quality + 1, 50);
+                    qualityAdjustement = 1;
                 }
                 else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    item.Quality = Math.Min(item.Quality + 1, 50);
-
-                    if (item.SellIn < 11)
-                    {
-                        item.Quality = Math.Min(item.Quality + 1, 50);
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        item.Quality = Math.Min(item.Quality + 1, 50);
-                    }
+                    if (item.SellIn > 10)
+                        qualityAdjustement = 1;
+                    else if (item.SellIn > 5)
+                        qualityAdjustement = 2;
+                    else if (item.SellIn > 0)
+                        qualityAdjustement = 3;
                 }
                 else if (item.Name == "Sulfuras, Hand of Ragnaros")
                 {
+                    qualityAdjustement = 0;
                 }
                 else
                 {
-                    item.Quality = Math.Max(item.Quality - 1, 0);
+                    qualityAdjustement = -1;
+                }
+
+                if (qualityAdjustement != 0)
+                {
+                    item.Quality = Math.Clamp(item.Quality + qualityAdjustement, 0, 50);
                 }
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                     item.SellIn -= 1;
                 }
+                
+                
 
                 if (item.SellIn < 0)
                 {
