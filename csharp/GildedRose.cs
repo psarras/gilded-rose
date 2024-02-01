83895e4f1b81a8edf3c66cb4239bb5456b72c537
spsarras <psarras.st@gmail.com>
2024-02-01 13:51:50 +0000

    fixed ifs

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -16,29 +16,30 @@ namespace csharp
         {
             foreach (var item in Items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name == "Aged Brie")
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = Math.Max(item.Quality - 1, 0);
-                    }
+                    item.Quality = Math.Min(item.Quality + 1, 50);
                 }
-                else
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     item.Quality = Math.Min(item.Quality + 1, 50);
 
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.SellIn < 11)
                     {
-                        if (item.SellIn < 11)
-                        {
-                            item.Quality = Math.Min(item.Quality + 1, 50);
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            item.Quality = Math.Min(item.Quality + 1, 50);
-                        }
+                        item.Quality = Math.Min(item.Quality + 1, 50);
                     }
+
+                    if (item.SellIn < 6)
+                    {
+                        item.Quality = Math.Min(item.Quality + 1, 50);
+                    }
+                }
+                else if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                }
+                else
+                {
+                    item.Quality = Math.Max(item.Quality - 1, 0);
                 }
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
@@ -48,23 +49,20 @@ namespace csharp
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Aged Brie")
+                    if (item.Name == "Aged Brie")
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = Math.Max(item.Quality - 1, 0);
-                            }
-                        }
-                        else
-                        {
-                            item.Quality -= item.Quality;
-                        }
+                        item.Quality = Math.Min(item.Quality + 1, 50);
+                    }
+                    else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        item.Quality -= item.Quality;
+                    }
+                    else if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    {
                     }
                     else
                     {
-                        item.Quality = Math.Min(item.Quality + 1, 50);
+                        item.Quality = Math.Max(item.Quality - 1, 0);
                     }
                 }
             }
